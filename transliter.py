b4ebdee1a60a0ca78a34d651c29ba10cb833fefe
Stepan <batbutbait@gmail.com>
2016-03-12 12:59:37 +0500

    cleaning

--- transliter.py
+++ transliter.py
@@ -1,5 +1,5 @@
 import os
-import re
+import glob
 import csv
 import zipfile
 
@@ -17,18 +17,15 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def allowed_file(filename):
-    print(22)
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
 def handle_file(f):
     if not allowed_file(f.filename):
-        print(11)
         return
     filename = secure_filename(f.filename)
     f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    print(12)
     return filename
 
 
@@ -39,23 +36,27 @@ def hello_world():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    files = request.files.getlist('file[]')
-    print(files)
-    if files:
-        print(2)
-        for file in files:
-            print(handle_file(file))
+    folders = [APP_STATIC+'/for_load/*', APP_STATIC+'/uploads/*', APP_STATIC+'/zip/*']
+    for del_files in folders:
+        files = glob.glob(del_files)
+        for f in files:
+            os.remove(f)
 
-    return redirect('/open')
+    files = request.files.getlist('file[]')
+    if files:
+        for file in files:
+            handle_file(file)
+    return redirect('/open', code=200)
 
 
 @app.route('/open')
 def opener():
+
+
     dict = {}
     files_name = []
     for root, dirs, files in os.walk(UPLOAD_FOLDER):
         for file in files:
-            print(files)
             files_name.append(file)
             with open(root+"/"+file, 'rt', encoding='utf-8') as r:
                 spam_reader = csv.reader(r)
@@ -70,8 +71,8 @@ def opener():
                     iter_line = line.split(';')
                     write_file.writerow(iter_line)
                 dict = {}
-    uploads = APP_STATIC+'/'
-    with zipfile.ZipFile(APP_STATIC+'/files.zip', 'w') as zip_file:
+    uploads = APP_STATIC+'/zip/'
+    with zipfile.ZipFile(APP_STATIC+'/zip/files.zip', 'w') as zip_file:
         for file in files_name:
             zip_file.write(APP_STATIC+'/for_load/'+file)
 
